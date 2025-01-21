bb0cee14b4d102ec78297ffb07282eb1067dea3f
DataPyPeil <peillon.clement74@gmail.com>
2025-01-21 17:23:39 +0100

    Corrected class

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -11,32 +11,18 @@ import matplotlib.pyplot as plt
 
 class LinearRegression():
     
-    def __init__(self, X, y_gt, lr=0.003, eps=1e-5, reg=None, std=None):
-
-        self.X = X
-        self.y_gt = y_gt
+    def __init__(self, lr=0.003, eps=1e-5):
+        if lr<=0:
+            raise ValueError(f'Learning rate must be strictly positive. You entered lr={lr}')
+        if eps<=0:
+            raise ValueError('Epsilon must be strictly positive. You entered eps={eps}')
+            
         self.lr = lr
         self.eps = eps
-        self.n, self.p = X.shape[0], X.shape[1]+1
-        self.params = np.random.rand(self.p, 1)
-        self.best_params = None
-        self.reg = reg
-        
-        # Add intercept
-        col = np.ones((self.n, 1))
-        self.X = np.concatenate((col, X), axis=1)
-        
-        # Standardisation
-        if std:
-            self.X = self._standardisation()
-            
-        # Initialisation
-        self.y_pred = self._evaluate()
-        self.J = [self._costFunction()]
-        self.J_grad = [self._gradients()]
+
         
         
-    def _evaluate(self):
+    def _evaluate(self, X):
         """
         Compute the model LINEAR REGRESSION
 
@@ -46,15 +32,15 @@ class LinearRegression():
             prediction
 
         """
-        return np.dot(self.X, self.params)
+        return np.dot(X, self.params)
     
     def _costFunction(self):
         """Compute cost function"""
-        return np.square(self.y_pred - self.y_gt).sum() / (2*self.y_gt.shape[0])
+        return np.square(self.y_pred - y_gt).sum() / (2*y_gt.shape[0])
     
-    def _regularizedCostFunction(self, reg='Lasso', penalty=0.1, penalty2=None):
+    def _regularizedCostFunction(self, regularization:str='Lasso', penalty:float=0.1, penalty2:float=0.2):
         """Compute cost function in case of regularized LinearRegression"""
-        reg_cap = reg.capitalize()
+        reg_cap = regularization.capitalize()
         if reg_cap=='LASSO':
             return self._costFunction() + penalty*np.abs(self.params).sum()
         elif reg_cap=='RIDGE':
@@ -65,28 +51,25 @@ class LinearRegression():
             else:
                 return self._costFunction() + penalty*np.square(self.params).sum() + penalty2*np.abs(self.params).sum()
         else:
-            ValueError(f'Invalid penalty name: {reg}\nPossibilities: "Ridge" or "Lasso"')
+            ValueError(f'Invalid penalty name: {regularization}\nPossibilities: "Ridge" or "Lasso"')
         
     def _gradients(self):
         """Calculate gradients"""
-        gradient = (((self.y_pred - self.y_gt) * self.X).sum(axis=0) / self.X.shape[0]).reshape(self.X.shape[1],1)
+        gradient = (((self.y_pred - y_gt) * X).sum(axis=0) / X.shape[0]).reshape(X.shape[1],1)
         return gradient
     
-    def _regularizedGradients(self, reg='Lasso', penalty=0.1, penalty2=0.1):
+    def _regularizedGradients(self, regularization:str='Lasso', penalty:float=0.1, penalty2:float=0.2):
         """Calculate gradients in case of regularized LinearRegression"""
         
-        reg_cap = reg.capitalize()
+        reg_cap = regularization.capitalize()
         if reg_cap=='LASSO':
             return self._gradients() + penalty*self.params.shape[0]
         elif reg_cap=='RIDGE':
             return self._gradients() + 2*penalty*self.params.sum()
         elif reg_cap=='ELASTIC':
-            if penalty2 is None:
-                ValueError('Invalid value for second penalty term')
-            else:
-                return self._gradients() + 2*penalty*self.params.sum + penalty2*self.params.shape[0]
+            return self._gradients() + 2*penalty*self.params.sum() + penalty2*self.params.shape[0]
         else:
-            ValueError(f'Invalid penalty name: {reg}\nPossibilities: "Ridge" or "Lasso" or "Elastic"')
+            ValueError(f'Invalid penalty name: {regularization}\nPossibilities: "Ridge" or "Lasso" or "Elastic"')
         
     def _evaluateUpdate(self):
         """Compute the change in gradient"""
@@ -96,80 +79,124 @@ class LinearRegression():
         """Update parameters"""
         return self.params - self.lr*self.J_grad[-1]
     
-    def _standardisation(self):
+    def _standardisation(X):
         "Standardisation of input values"
-        return (self.X - np.mean(self.X)) / np.std(self.X)
+        return (X - np.mean(X)) / np.std(X)
     
-    def _normalisationMinMax(self):
-        """Normalisation of input values"""
-        return ((self.X - np.min(self.X))/np.max(self.X)-np.min(self.X))
+    # def _normalisationMinMax(X):
+    #     """Normalisation of input values"""
+    #     return ((X - np.min(X))/np.max(X)-np.min(X))
         
-    def fit(self):
+    def fit(self, X, y_gt, y_intercept:bool=True, standardize:bool=False, regularization:str=None):
         """
-        Model fit
+        Fit the model to X and y_gt data
+
+        Parameters
+        ----------
+        X : ndarray
+            Features.
+        y_gt : ndarray
+            Targets.
+        y_intercept : bool, optional
+            Add a bias to the linear regression model.
+            The default is True.
+        standardize : bool, optional
+            Standardize X matrix before fitting.
+            The default is False.
+        regularization : str, optional
+            DESCRIPTION. The default is None.
 
         Returns
         -------
         None.
 
         """
+
+        self.n, self.p = X.shape[0], X.shape[1]+1
+        self.params = np.random.rand(self.p, 1)
+        
+        # Add intercept
+        if y_intercept:
+            col = np.ones((self.n, 1))
+            X = np.concatenate((col, X), axis=1)
+        
+        # Standardize if necessary
+        if standardize:
+            X = X._standardisation(X)
+            
         diff = [self.eps + 1]
         count = 0
         
-        if self.reg is None:
+        if self.regularization is None:
+            # Initialisation
+            self.y_pred = self._evaluate(X)
+            self.J = [self._costFunction(y_gt)]
+            self.J_grad = [self._gradients(X, y_gt)]
+            
+            # Update weights
             while diff[-1] > self.eps:
                 if count%1000==0:
                     print(f'\n--- Itération {count} ---\nMSE = {self.J[-1]:.7E}')
                 count += 1
                 self.params = self._updateParams()
-                self.y_pred = self._evaluate()
-                self.J.append(self._costFunction())
-                self.J_grad.append(self._gradients())
+                self.y_pred = self._evaluate(X)
+                self.J.append(self._costFunction(y_gt))
+                self.J_grad.append(self._gradients(X, y_gt))
                 diff.append(self._evaluateUpdate())
         
         else:
+            # Initialisation
+            self.y_pred = self._evaluate(X)
+            self.J = [self._regularizedCostFunction(y_gt, regularization)]
+            self.J_grad = [self._regularizedGradients(X, y_gt)]
+            
             while diff[-1] > self.eps:
                 if count%1000==0:
                     print(f'\n--- Itération {count} ---\nMSE = {self.J[-1]:.7E}')
                 count += 1
                 self.params = self._updateParams()
-                self.y_pred = self._evaluate()
-                self.J.append(self._regularizedCostFunction())
-                self.J_grad.append(self._gradients())
+                self.y_pred = self._evaluate(X)
+                self.J.append(self._regularizedCostFunction(y_gt))
+                self.J_grad.append(self._regularizedGradients(X, y_gt))
                 diff.append(self._evaluateUpdate())
         
         self.best_params = self.params.copy()
         print(f'\nFit process ended with success\nMSE = {self.J[-1]}\n')
     
-    def predict(self):
+    def predict(self, X):
         """
         Make predictions
 
+        Parameters
+        ----------
+        X : ndarray
+            Input data to make predictions from.
+
         Returns
         -------
-        y-value
-            Prediction of the model as is.
+        ndarray
+            Model prediction.
         """
-        return np.dot(self.X, self.params)
+        return np.dot(X, self.params)
     
-    def score(self):
+    def score(y_gt, y_pred):
         """
-        Compute coefficient of determination R-squared
+        Compute coefficient of determination
 
         Parameters
         ----------
         y_gt : ndarray
-            Target to predict = Ground Truth.
+            Real targets.
+        y_pred : ndarray
+            Model predictions.
 
         Returns
         -------
-        float
-            R-squared.
+        np.float
+            R-squared i.e. coefficient of determination.
         """
-        r_squared = 1-(np.sum((self.y_gt-self.y_pred)**2)/np.sum((self.y_gt-np.mean(self.y_gt))**2))
-        print(f'R-squared: {r_squared:.04f}')
         
-        return r_squared
+        return 1-(np.sum((y_gt-y_pred)**2)/np.sum((y_gt-np.mean(y_gt))**2))
     
     def plot(self):
         """
@@ -188,7 +215,7 @@ class LinearRegression():
         plt.xlabel('Itération')
         plt.legend()
         
-    def plot_featureimportance(self, names=None):
+    def featureimportance(self, names=None):
         """
         Plot the features by their importance in the model
 
@@ -220,7 +247,26 @@ class LinearRegression():
         plt.barh(names_modified, p_modif, color=['navy', 'darkorange'])
         plt.show()
         
-    def plot_actualVSpredicted(y_gt, y):
+    def plot_actualVSpredicted(y_gt, y_pred):
+        """
+        Visual representation of the performance of the linear model
+
+        Parameters
+        ----------
+        y_gt : ndarray
+            Real targets.
+        y_pred : ndarray
+            Predictions.
+
+        Returns
+        -------
+        None.
+
+        """
+        plt.figure(figsize=(7,7))
+        plt.title('Actual vs Predicted')
+        plt.scatter(y_gt, y_pred, alpha=0.8, s=10, color='navy')
+        plt.plot([-1000,1000], [-1000,1000], lw=1, ls='-', color='darkorange')
         return None
 
 """ Only keep hyperparameters in __init__ like penalty parameters ..."""
